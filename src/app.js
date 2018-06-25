887907cc6b02edbb6596b70487558272d9c6184b
IshanArya <ishan.arya130@gmail.com>
2018-06-24 20:50:54 -0400

    Updated something...

--- src/app.js
+++ src/app.js
@@ -12,125 +12,125 @@ console.log("Start!");
 const appDirectory = 'file://' + __dirname + '/';
 const baseUrl = "http://saavn.com";
 const menuTemplate = [
+{
+	label: 'Electron',
+	submenu: [
 	{
-		label: 'Electron',
-		submenu: [
-			{
-				label: 'About Electron',
-				selector: 'orderFrontStandardAboutPanel:'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				label: 'Services',
-				submenu: []
-			},
-			{
-				type: 'separator'
-			},
-			{
-				label: 'Hide Electron',
-				accelerator: 'Command+H',
-				selector: 'hide:'
-			},
-			{
-				label: 'Hide Others',
-				accelerator: 'Command+Shift+H',
-				selector: 'hideOtherApplications:'
-			},
-			{
-				label: 'Show All',
-				selector: 'unhideAllApplications:'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				label: 'Quit',
-				accelerator: 'Command+Q',
-				click: function() { app.quit(); }
-			},
-		]
+		label: 'About Electron',
+		selector: 'orderFrontStandardAboutPanel:'
 	},
 	{
-		label: 'Edit',
-		submenu: [
-			{
-				label: 'Undo',
-				accelerator: 'Command+Z',
-				selector: 'undo:'
-			},
-			{
-				label: 'Redo',
-				accelerator: 'Shift+Command+Z',
-				selector: 'redo:'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				label: 'Cut',
-				accelerator: 'Command+X',
-				selector: 'cut:'
-			},
-			{
-				label: 'Copy',
-				accelerator: 'Command+C',
-				selector: 'copy:'
-			},
-			{
-				label: 'Paste',
-				accelerator: 'Command+V',
-				selector: 'paste:'
-			},
-			{
-				label: 'Select All',
-				accelerator: 'Command+A',
-				selector: 'selectAll:'
-			},
-		]
+		type: 'separator'
 	},
 	{
-		label: 'View',
-		submenu: [
-			{role: 'reload'},
-			{role: 'forcereload'},
-			{role: 'toggledevtools'},
-			{type: 'separator'},
-			{role: 'resetzoom'},
-			{role: 'zoomin'},
-			{role: 'zoomout'},
-			{type: 'separator'},
-			{role: 'togglefullscreen'}
-		]
-	},
-	{
-		label: 'Window',
-		submenu: [
-			{
-				label: 'Minimize',
-				accelerator: 'Command+M',
-				selector: 'performMiniaturize:'
-			},
-			{
-				label: 'Close',
-				accelerator: 'Command+W',
-				selector: 'performClose:'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				label: 'Bring All to Front',
-				selector: 'arrangeInFront:'
-			},
-		]
-	},
-	{
-		label: 'Help',
+		label: 'Services',
 		submenu: []
-	}
+	},
+	{
+		type: 'separator'
+	},
+	{
+		label: 'Hide Electron',
+		accelerator: 'Command+H',
+		selector: 'hide:'
+	},
+	{
+		label: 'Hide Others',
+		accelerator: 'Command+Shift+H',
+		selector: 'hideOtherApplications:'
+	},
+	{
+		label: 'Show All',
+		selector: 'unhideAllApplications:'
+	},
+	{
+		type: 'separator'
+	},
+	{
+		label: 'Quit',
+		accelerator: 'Command+Q',
+		click: function() { app.quit(); }
+	},
+	]
+},
+{
+	label: 'Edit',
+	submenu: [
+	{
+		label: 'Undo',
+		accelerator: 'Command+Z',
+		selector: 'undo:'
+	},
+	{
+		label: 'Redo',
+		accelerator: 'Shift+Command+Z',
+		selector: 'redo:'
+	},
+	{
+		type: 'separator'
+	},
+	{
+		label: 'Cut',
+		accelerator: 'Command+X',
+		selector: 'cut:'
+	},
+	{
+		label: 'Copy',
+		accelerator: 'Command+C',
+		selector: 'copy:'
+	},
+	{
+		label: 'Paste',
+		accelerator: 'Command+V',
+		selector: 'paste:'
+	},
+	{
+		label: 'Select All',
+		accelerator: 'Command+A',
+		selector: 'selectAll:'
+	},
+	]
+},
+{
+	label: 'View',
+	submenu: [
+	{role: 'reload'},
+	{role: 'forcereload'},
+	{role: 'toggledevtools'},
+	{type: 'separator'},
+	{role: 'resetzoom'},
+	{role: 'zoomin'},
+	{role: 'zoomout'},
+	{type: 'separator'},
+	{role: 'togglefullscreen'}
+	]
+},
+{
+	label: 'Window',
+	submenu: [
+	{
+		label: 'Minimize',
+		accelerator: 'Command+M',
+		selector: 'performMiniaturize:'
+	},
+	{
+		label: 'Close',
+		accelerator: 'Command+W',
+		selector: 'performClose:'
+	},
+	{
+		type: 'separator'
+	},
+	{
+		label: 'Bring All to Front',
+		selector: 'arrangeInFront:'
+	},
+	]
+},
+{
+	label: 'Help',
+	submenu: []
+}
 ];
 if (process.platform === 'darwin') {
 	menuTemplate.unshift({
@@ -198,12 +198,12 @@ ipc.on('download-song', (e, song) => {
 		 * }
 		 */
 
-		mainWindow.setProgressBar(progress.transferred/progress.total);
-		mainWindow.webContents.send('download-progress', {
-			song_id: song_id,
-			progress: progress
-		});
-	})
+		 mainWindow.setProgressBar(progress.transferred/progress.total);
+		 mainWindow.webContents.send('download-progress', {
+		 	song_id: song_id,
+		 	progress: progress
+		 });
+		})
 	.then(() => {
 		console.log("Downloaded: " + mp3Name);
 
